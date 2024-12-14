58a062ef3c07e06e3fdadfb9dbdad056a7020be1
donghyunk <gnfleh2001@naver.com>
2024-12-14 19:29:29 +0900

    상점무기추가

--- GameStart/src/ShopPanel.java
+++ GameStart/src/ShopPanel.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 
 public class ShopPanel extends JPanel {
     private GameManager manager;
+    private int[] weaponID = {1,2,3,4,5};
+    private int yPosition = 100;
 
     public ShopPanel(GameManager manager) throws IOException {
         this.manager = manager;
@@ -15,32 +17,68 @@ public class ShopPanel extends JPanel {
         title.setBounds(50, 20, 300, 50);
         add(title);
 
-        // Weapon buttons
-        int yPosition = 100;
-        for (int i = 1; i <= 1; i++) {
-            int weaponID = i;
-            System.out.println(getClass().getResource("Weapon/weapon-Sheet1.png"));
-            // 무기 스프라이트와 총알 프레임 준비
-            BufferedImage[] weaponSprites = loadSpriteSheet("/Weapon/weapon-Sheet"+ weaponID +".png", 72, 72);
-            BufferedImage[] bulletFrames = loadSpriteSheet("/Weapon/bullet-Sheet"+ weaponID +".png", 6, 4);       
+        // 무기 스프라이트와 총알 프레임 준비
+        BufferedImage[] weaponSprites1 = loadSpriteSheet("/Weapon/weapon-Sheet"+ weaponID[0] +".png", 72, 72);
+        BufferedImage[] bulletFrames1 = loadSpriteSheet("/Weapon/bullet-Sheet"+ weaponID[0] +".png", 6, 4);   
+        
+        BufferedImage[] weaponSprites2 = loadSpriteSheet("/Weapon/weapon-Sheet"+ weaponID[1] +".png", 72, 72);
+        BufferedImage[] bulletFrames2 = loadSpriteSheet("/Weapon/bullet-Sheet"+ weaponID[1] +".png", 24, 30);    
+        
+        BufferedImage[] weaponSprites3 = loadSpriteSheet("/Weapon/weapon-Sheet"+ weaponID[2] +".png", 72, 72);
+        BufferedImage[] bulletFrames3 = loadSpriteSheet("/Weapon/bullet-Sheet"+ weaponID[2] +".png", 24, 30);    
 
-            JButton weaponButton = new JButton("Weapon " + weaponID + " - Cost: " + (weaponID * 100) + " Gold");
-            weaponButton.setBounds(50, yPosition, 300, 50);
+        
+        
+        JButton weaponButton1 = new JButton("Weapon - Cost: " + (weaponID[0]) + " Gold");
+        weaponButton1.setBounds(50, yPosition, 300, 50);
 
-            weaponButton.addActionListener(e -> {
-                Player player = manager.getPlayer();
-                if (player.getGold() >= weaponID * 100) {
-                    player.addGold(-(weaponID * 100));
-                    player.setWeapon(new Weapon(weaponSprites[0], 3, weaponID * 10, 300, bulletFrames));
-                    JOptionPane.showMessageDialog(this, "Weapon " + weaponID + " purchased!");
-                } else {
-                    JOptionPane.showMessageDialog(this, "Not enough gold!");
-                }
-            });
+        weaponButton1.addActionListener(e -> {
+            Player player = manager.getPlayer();
+            if (player.getGold() >= weaponID[0] * 100) {
+                player.addGold(-(weaponID[0] * 100));
+                player.setWeapon(new Weapon(weaponSprites1[0], 3, weaponID[0] * 100, 20, bulletFrames1));
+                JOptionPane.showMessageDialog(this, "Weapon purchased!");
+            } else {
+                JOptionPane.showMessageDialog(this, "Not enough gold!");
+            }
+        });
+        
+        add(weaponButton1);
+        yPosition += 60;
+        
+        JButton weaponButton2 = new JButton("Weapon - Cost: " + (weaponID[1] * 100) + " Gold");
+        weaponButton2.setBounds(50, yPosition, 300, 50);
 
-            add(weaponButton);
-            yPosition += 60;
-        }
+        weaponButton2.addActionListener(e -> {
+            Player player = manager.getPlayer();
+            if (player.getGold() >= weaponID[1] * 100) {
+                player.addGold(-( weaponID[1] * 100));
+                player.setWeapon(new Weapon(weaponSprites2[0], 3,  weaponID[1] * 100, 20, bulletFrames2));
+                JOptionPane.showMessageDialog(this, "Weapon purchased!");
+            } else {
+                JOptionPane.showMessageDialog(this, "Not enough gold!");
+            }
+        });
+        
+        add(weaponButton2);
+        yPosition += 60;
+        
+        JButton weaponButton3 = new JButton("Weapon - Cost: " + (weaponID[2] * 100) + " Gold");
+        weaponButton3.setBounds(50, yPosition, 300, 50);
+
+        weaponButton3.addActionListener(e -> {
+            Player player = manager.getPlayer();
+            if (player.getGold() >= weaponID[2] * 100) {
+                player.addGold(-(weaponID[2] * 100));
+                player.setWeapon(new Weapon(weaponSprites3[0], 3, weaponID[2] * 100, 20, bulletFrames3));
+                JOptionPane.showMessageDialog(this, "Weapon purchased!");
+            } else {
+                JOptionPane.showMessageDialog(this, "Not enough gold!");
+            }
+        });
+        
+        add(weaponButton3);
+        yPosition += 30;
 
         // Go back to lobby
         JButton lobbyButton = new JButton("Go to Lobby");

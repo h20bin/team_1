09824c896b19d049488e350cce49c54b58ede05f
h20bin <csb6875@naver.com>
2024-12-14 17:20:03 +0900

    Add files via upload

--- GameStart/src/LobbyPanel.java
+++ GameStart/src/LobbyPanel.java
@@ -1,39 +1,34 @@
 import javax.swing.*;
 import java.awt.*;
-import java.io.IOException;
 
 public class LobbyPanel extends JPanel {
     private GameManager manager;
+    private ImageIcon lobbybackgroundImageIcon;
 
     public LobbyPanel(GameManager manager) {
         this.manager = manager;
         setLayout(null);
+        
+        lobbybackgroundImageIcon = new ImageIcon(getClass().getResource("/background/lobbyback.jpeg"));
 
         JLabel playerLabel = new JLabel("Player");
         playerLabel.setHorizontalAlignment(SwingConstants.CENTER);
         playerLabel.setFont(new Font("Arial", Font.BOLD, 24));
-        playerLabel.setBounds(180, 50, 200, 30);
+        playerLabel.setBounds(150, 50, 200, 30);
         add(playerLabel);
 
         JButton shopButton = new JButton("Shop");
-        shopButton.setBounds(80, 250, 100, 50);
-        shopButton.addActionListener(e -> {
-			try {
-				manager.switchPanel(new ShopPanel(manager));
-			} catch (IOException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-		});
+        shopButton.setBounds(100, 250, 100, 50);
+        shopButton.addActionListener(e -> manager.switchPanel(new ShopPanel(manager)));
         add(shopButton);
 
         JButton stageButton = new JButton("Stage");
-        stageButton.setBounds(180, 250, 100, 50);
+        stageButton.setBounds(200, 250, 100, 50);
         stageButton.addActionListener(e -> manager.switchPanel(new StagePanel(manager)));
         add(stageButton);
 
         JButton forgeButton = new JButton("Forge");
-        forgeButton.setBounds(280, 250, 100, 50);
+        forgeButton.setBounds(300, 250, 100, 50);
         forgeButton.addActionListener(e -> manager.switchPanel(new ForgePanel(manager)));
         add(forgeButton);
     }
@@ -43,6 +38,15 @@ public class LobbyPanel extends JPanel {
         super.paintComponent(g);
         g.setColor(Color.LIGHT_GRAY);
         g.fillRect(0, 0, getWidth(), getHeight());
+        
+        if (lobbybackgroundImageIcon != null) {
+            Image backgroundImage = lobbybackgroundImageIcon.getImage(); // ImageIcon에서 Image 객체 추출
+            g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this); // 배경 이미지를 패널 크기에 맞게 그리기
+        } else {
+            // 이미지가 로드되지 않았을 경우 검정색 배경
+            g.setColor(Color.BLACK);
+            g.fillRect(0, 0, getWidth(), getHeight());
+        }
 
         // HUD 표시
         Player player = manager.getPlayer();
